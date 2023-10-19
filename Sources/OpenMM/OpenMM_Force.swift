a87d981c5dbf80e5038e57b065c0d5f66951ccd6
Philip Turner <philipturner.AR@gmail.com>
2023-10-18 21:49:55 -0400

    Add external force

--- Sources/OpenMM/OpenMM_Force.swift
+++ Sources/OpenMM/OpenMM_Force.swift
@@ -138,6 +138,53 @@ public class OpenMM_CustomCompoundBondForce: OpenMM_Force {
   }
 }
 
+public class OpenMM_CustomExternalForce: OpenMM_Force {
+  public init(energy: String) {
+    super.init(_openmm_create(energy, OpenMM_CustomExternalForce_create))
+    self.retain()
+  }
+  
+  public override class func destroy(_ pointer: OpaquePointer) {
+    OpenMM_CustomExternalForce_destroy(pointer)
+  }
+  
+  @discardableResult
+  public func addGlobalParameter(
+    name: String, defaultValue: Double
+  ) -> Int {
+    let index = OpenMM_CustomExternalForce_addGlobalParameter(
+      pointer, name, defaultValue)
+    return Int(index)
+  }
+  
+  @discardableResult
+  public func addParticle(
+    _ particle: Int, parameters: OpenMM_DoubleArray
+  ) -> Int {
+    let index = OpenMM_CustomExternalForce_addParticle(
+      pointer, Int32(particle), parameters.pointer)
+    return Int(index)
+  }
+  
+  @discardableResult
+  public func addPerParticleParameter(
+    name: String
+  ) -> Int {
+    let index = OpenMM_CustomExternalForce_addPerParticleParameter(
+      pointer, name)
+    return Int(index)
+  }
+  
+  // There is no simple way to wrap this in a Swift computed property.
+  public func setParticleParameters(
+    index: Int, particle: Int, parameters: OpenMM_DoubleArray
+  ) {
+    OpenMM_CustomExternalForce_setParticleParameters(
+      pointer, Int32(truncatingIfNeeded: index),
+      Int32(truncatingIfNeeded: particle), parameters.pointer)
+  }
+}
+
 public class OpenMM_CustomHbondForce: OpenMM_Force {
   public struct NonbondedMethod: OptionSet {
     public var rawValue: UInt32
